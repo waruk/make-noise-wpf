261f256c636b5ee68fedac4f4aabe12cab692925
Andrei C <aciurea@hotmail.com>
2018-11-07 23:11:55 +0200

    Add logging & quiet time

---
+++ Noise/NoiseConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Noise
+{
+    public class NoiseConfiguration
+    {
+        QuietTime quietHours { get; set; }
+        int PlayDuration { get; set; }
+        Sound MediaFile { get; set; }
+    }
+}
