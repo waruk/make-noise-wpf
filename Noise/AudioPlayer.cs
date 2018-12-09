0591ebc574625a5208db1ff2980c83a22ff03aba
Andrei Ciurea <aciurea@outlook.com>
2018-12-09 16:31:07 +0200

    added play/stop icons; moved code in AudioPlayer class.

--- Noise/AudioPlayer.cs
+++ Noise/AudioPlayer.cs
@@ -1,16 +1,104 @@
-﻿using System.Windows.Media;
+﻿using NLog;
 using System;
-using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace Noise
 {
-    public class AudioPlayer
+    public class AudioPlayer : IDisposable
     {
-        
+        private const string SOUND_LIB_DIR = @".\Sounds";
+        private const string SOUND1 = @".\Sounds\Icebraker.mp3";
+        private const int PLAY_DURATION = 60;
 
-        public void Play()
+        private static Random rnd = new Random();
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        // controls when media is played
+        private DispatcherTimer playIntervalTimer = new DispatcherTimer();
+
+        // controls how much of the media is played
+        private DispatcherTimer playDurationTimer = new DispatcherTimer();
+
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        private bool isMediaPlaying;
+
+        public QuietTime QuietHours { get; set; }
+
+        public void Start()
         {
-            
+            mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
+            mediaPlayer.Volume = 1;
+
+            playDurationTimer.Tick += playDurationTimer_Tick;
+            playDurationTimer.Interval = TimeSpan.FromSeconds(PLAY_DURATION);
+
+            playIntervalTimer.Tick += playIntervalTimer_Tick;
+            playIntervalTimer.Interval = TimeSpan.FromSeconds(5);
+            //playIntervalTimer.Interval = TimeSpan.FromMinutes(120);
+            playIntervalTimer.Start();
+        }
+
+        private void playIntervalTimer_Tick(object sender, EventArgs e)
+        {
+            // check if we're allowed to play
+            DateTime currentTime = DateTime.Now;
+            if (QuietHours.IsInsideQuietTimeInterval(currentTime))
+            {
+                logger.Info("Inside quiet hours interval. Keep quiet!");
+                return;
+            }
+
+            if (!isMediaPlaying)
+            {
+                mediaPlayer.Open(new Uri(SOUND1, UriKind.Relative));
+
+                mediaPlayer.Play();
+                isMediaPlaying = true;
+                logger.Info("Media started playing.");
+
+                playDurationTimer.Start();
+            }
+        }
+
+        private void playDurationTimer_Tick(object sender, EventArgs e)
+        {
+            if (isMediaPlaying)
+            {
+                mediaPlayer.Stop();
+                isMediaPlaying = false;
+                logger.Info("Media was stopped because max play length elapsed.");
+            }
+            ResetPlayInterval();
+        }
+
+        private void MediaPlayer_MediaEnded(object sender, EventArgs e)
+        {
+            logger.Info("Media ended.");
+            isMediaPlaying = false;
+            ResetPlayInterval();
+        }
+
+        private void ResetPlayInterval()
+        {
+            playIntervalTimer.Interval = TimeSpan.FromSeconds(10);
+            playDurationTimer.Stop();
+            logger.Info("Media will be started again after 10 seconds.");
+        }
+
+        private TimeSpan GetNextPlayTime()
+        {
+            int minutes = rnd.Next(60, 91);
+            TimeSpan timeBetweenMediaPlay = TimeSpan.FromMinutes(minutes);
+
+            return timeBetweenMediaPlay;
+        }
+
+        public void Dispose()
+        {
+            mediaPlayer.MediaEnded -= MediaPlayer_MediaEnded;
+            playIntervalTimer.Tick -= playIntervalTimer_Tick;
+            playDurationTimer.Tick -= playDurationTimer_Tick;
         }
     }
 }
--- Noise/MainWindow.xaml.cs
+++ Noise/MainWindow.xaml.cs
@@ -18,8 +18,6 @@
 using System;
 using System.Configuration;
 using System.Windows;
-using System.Windows.Media;
-using System.Windows.Threading;
 using NLog;
 
 namespace Noise
@@ -29,117 +27,63 @@ namespace Noise
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string SOUND_LIB_DIR = @".\Sounds";
-        private const string SOUND1 = @".\Sounds\sample1.mp3";
-        private const int PLAY_DURATION = 60;
-
-        private static Random rnd = new Random();
         // initialise logger
         private static Logger logger = LogManager.GetCurrentClassLogger();
-
-        // controls when media is played
-        private DispatcherTimer playIntervalTimer = new DispatcherTimer();
-        
-        // controls how much of the media is played
-        private DispatcherTimer playDurationTimer = new DispatcherTimer();
-
-        private MediaPlayer mediaPlayer = new MediaPlayer();
-        private bool isMediaPlaying;
-
-        QuietTime quietHours;
+        private AudioPlayer audioPlayer;
+        private bool isPlayerStarted = false;
 
         public MainWindow()
         {
             InitializeComponent();
             
             logger.Info("App started.");
+        }
+
+        private void BtnStartPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlayerStarted)
+            {
+                audioPlayer.Dispose();
+                audioPlayer = null;
+                logger.Info("Audio player stopped");
+                btnStartPlayer.Content = FindResource("StartButton");
+                isPlayerStarted = false;
+
+                return;
+            }
+
+            audioPlayer = new AudioPlayer();
+
             try
             {
                 TimeSpan quietTimeFrom = TimeSpan.Parse(ConfigurationManager.AppSettings["QuietHoursFrom"]);
                 TimeSpan quietTimeTo = TimeSpan.Parse(ConfigurationManager.AppSettings["QuietHoursTo"]);
-                quietHours = new QuietTime(quietTimeFrom, quietTimeTo);
+                audioPlayer.QuietHours = new QuietTime(quietTimeFrom, quietTimeTo);
+
+                audioPlayer.Start();
+                logger.Info("Audio player started");
+
+                isPlayerStarted = true;
+                btnStartPlayer.Content = FindResource("StopButton");
+            }
+            catch (ArgumentException ex)
+            {
+                logger.Error(ex, "Quiet time interval is not valid.");
+                MessageBox.Show("Quiet time interval is not valid.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             catch (Exception ex)
             {
-                logger.Error(ex, "Quiet time interval is not valid.");
+                logger.Error(ex);
+                MessageBox.Show("An unknown error has occurred.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-            mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
-            mediaPlayer.Volume = 1;
-
-            playDurationTimer.Tick += playDurationTimer_Tick;
-            playDurationTimer.Interval = TimeSpan.FromSeconds(PLAY_DURATION);
-
-            playIntervalTimer.Tick += playIntervalTimer_Tick;
-            playIntervalTimer.Interval = TimeSpan.FromSeconds(5);
-            //playIntervalTimer.Interval = TimeSpan.FromMinutes(120);
-            playIntervalTimer.Start();
-        }
-
-        private void playIntervalTimer_Tick(object sender, EventArgs e)
-        {
-            // check if we're allowed to play
-            DateTime currentTime = DateTime.Now;
-            if (quietHours.IsInsideQuietTimeInterval(currentTime))
-            {
-                logger.Info("Inside quiet hours interval. Keep quiet!");
-                return;
-            }
-
-            if (!isMediaPlaying)
-            {
-                mediaPlayer.Open(new Uri(SOUND1, UriKind.Relative));
-                
-                mediaPlayer.Play();
-                isMediaPlaying = true;
-                logger.Info("Media started playing.");
-
-                playDurationTimer.Start();
-            }
-        }
-
-        private void playDurationTimer_Tick(object sender, EventArgs e)
-        {
-            if (isMediaPlaying)
-            {
-                mediaPlayer.Stop();
-                isMediaPlaying = false;
-                logger.Info("Media was stopped because max play length elapsed.");
-            }
-            ResetPlayInterval();
-        }
-
-        private void MediaPlayer_MediaEnded(object sender, EventArgs e)
-        {
-            logger.Info("Media ended.");
-            isMediaPlaying = false;
-            ResetPlayInterval();
-        }
-
-        private void ResetPlayInterval()
-        {
-            playIntervalTimer.Interval = TimeSpan.FromSeconds(10);
-            playDurationTimer.Stop();
-            logger.Info("Media will be started again after 10 seconds.");
-        }
-
-        private TimeSpan GetNextPlayTime()
-        {
-            int minutes = rnd.Next(60, 91);
-            TimeSpan timeBetweenMediaPlay = TimeSpan.FromMinutes(minutes);
-
-            return timeBetweenMediaPlay;
         }
 
         // clean up when shutting down the app
         private void Window_Closed(object sender, EventArgs e)
         {
             logger.Info("App is shutting down.");
-
-            mediaPlayer.MediaEnded -= MediaPlayer_MediaEnded;
-            playIntervalTimer.Tick -= playIntervalTimer_Tick;
-            playDurationTimer.Tick -= playDurationTimer_Tick;
-
+            if (audioPlayer != null)
+                audioPlayer.Dispose();
             LogManager.Shutdown(); // Flush and close down internal threads and timers
         }
     }
