1e88da33a8a694a287cb346772da69184cedad2a
Andrei Ciurea <aciurea@outlook.com>
2018-12-27 15:52:44 +0200

    Added noise web client (ported wpf to the web).

---
+++ ControlCenter/scripts/make-noise.js
@@ -0,0 +1,49 @@
+﻿var isPlayerStarted = false;
+
+showPlayerControls("play");
+
+function showPlayerControls(buttonToShow) {
+    playButton = document.querySelector('#play-button');
+    stopButton = document.querySelector('#stop-button');
+
+    if (buttonToShow === "play") {
+        playButton.className = "show-player-button";
+        stopButton.className = "hide-player-button";
+    }
+    else {
+        playButton.className = "hide-player-button";
+        stopButton.className = "show-player-button";
+    }
+
+}
+
+function setPlayerStatus(status) {
+    playerStatusElement = document.querySelector('#player-status');
+    playerStatusElement.textContent = "Status " + status;
+}
+
+function writeToLog(message) {
+    logElement = document.querySelector("#plaplayer-log")
+    
+
+}
+
+/* attach events */
+
+document.querySelector("#player-controls").onclick = function () {
+    if (isPlayerStarted) {
+        setPlayerStatus("OFF");
+        showPlayerControls("play");
+        isPlayerStarted = false;
+    }
+    else {
+        setPlayerStatus("ON");
+        showPlayerControls("stop");
+        isPlayerStarted = true;
+    }
+}
+
+
+
+
+
