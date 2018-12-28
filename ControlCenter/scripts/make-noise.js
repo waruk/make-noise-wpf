6a1b57c4661256523fce2719efc26f0e5957cbb8
Andrei Ciurea <aciurea@outlook.com>
2018-12-28 17:09:25 +0200

    Added interval play, max length play, on page logging.

--- ControlCenter/scripts/make-noise.js
+++ ControlCenter/scripts/make-noise.js
@@ -1,49 +1,97 @@
-﻿var isPlayerStarted = false;
+﻿var playIntervalId;
+var playLengthId;
+var minIntervalValue;
+var maxIntervalValue;
+var maxLengthToPlay;
 
-showPlayerControls("play");
 
-function showPlayerControls(buttonToShow) {
-    playButton = document.querySelector('#play-button');
-    stopButton = document.querySelector('#stop-button');
+function logInfo(message, important) {
+    console.log(message);
 
-    if (buttonToShow === "play") {
-        playButton.className = "show-player-button";
-        stopButton.className = "hide-player-button";
+    logElement = document.getElementById("player-log");
+    newLine = document.createElement("p");
+    if (important)
+        newLine.classList.add("bold-red-text");
+
+    currentDate = Date.now();
+    dateFormat = {
+        year: 'numeric', month: 'numeric', day: 'numeric',
+        hour: 'numeric', minute: 'numeric', second: 'numeric'
+    }
+    newLine.innerText = new Intl.DateTimeFormat('ro-RO', dateFormat).format(currentDate) + " : " + message;
+    logElement.insertBefore(newLine, logElement.firstChild);
+
+    // also save log to db via web API
+}
+
+function readConfiguration() {
+    minIntervalValue = parseInt(document.getElementById("min-value").value);
+    maxIntervalValue = parseInt(document.getElementById("max-value").value);
+    maxLengthToPlay = parseInt(document.getElementById("max-length").value);
+    if (Number.isInteger(minIntervalValue) && Number.isInteger(maxIntervalValue) && Number.isInteger(maxLengthToPlay)) {
+        return true;
+    }
+}
+
+async function playSound() {
+    audioElem = document.getElementById("audio-elem");
+    try {
+        await audioElem.play();
+        logInfo("Playing sound file!");
+        playLengthId = window.setTimeout(stopPlay, maxLengthToPlay * 1000);
+    } catch (err) {
+        logInfo("Can not play the file.");
+    }
+}
+
+function stopPlay() {
+    audioElem = document.getElementById("audio-elem");
+    audioElem.pause();
+    audioElem.currentTime = 0;
+    logInfo("Max play length reached.");
+    scheduleNextPlay();
+}
+
+function getNextPlayTime(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+function scheduleNextPlay() {
+    nextPlayTime = getNextPlayTime(minIntervalValue, maxIntervalValue);  // time in minutes
+    logInfo("Next play in: " + nextPlayTime + " minutes");
+    playIntervalId = window.setTimeout(playSound, nextPlayTime * 60000); // time in seconds
+}
+
+
+/* event handlers */
+function playButtonClick() {
+    if (readConfiguration()) {
+        logInfo("AudioPlayer started.", true);
+        scheduleNextPlay();
     }
     else {
-        playButton.className = "hide-player-button";
-        stopButton.className = "show-player-button";
+        logInfo("Configurion is invalid.");
     }
-
 }
 
-function setPlayerStatus(status) {
-    playerStatusElement = document.querySelector('#player-status');
-    playerStatusElement.textContent = "Status " + status;
+function stopButtonClick() {
+    window.clearTimeout(playIntervalId);
+    window.clearTimeout(playLengthId);
+    audioElem = document.getElementById("audio-elem");
+    audioElem.pause();
+    audioElem.currentTime = 0;
+    logInfo("AudioPlayer stopped.", true);
 }
 
-function writeToLog(message) {
-    logElement = document.querySelector("#plaplayer-log")
-    
-
+function audioEnded() {
+    logInfo("Playback ended.");
+    scheduleNextPlay();
 }
+
 
 /* attach events */
-
-document.querySelector("#player-controls").onclick = function () {
-    if (isPlayerStarted) {
-        setPlayerStatus("OFF");
-        showPlayerControls("play");
-        isPlayerStarted = false;
-    }
-    else {
-        setPlayerStatus("ON");
-        showPlayerControls("stop");
-        isPlayerStarted = true;
-    }
-}
-
-
-
-
-
+document.getElementById("play-button").addEventListener("click", playButtonClick);
+document.getElementById("stop-button").addEventListener("click", stopButtonClick);
+document.getElementById('audio-elem').addEventListener("ended", audioEnded);
